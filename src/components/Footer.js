078b7a17a87b92da0f7fae857beed75a22d8c2e0
kiran364 <79898690+kiran364@users.noreply.github.com>
2021-08-12 16:48:18 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,20 +9,6 @@ function Footer() {
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
         You can’t stop time, but you can save it!</p>
-        {/* <p className='footer-subscription-text'>
-            You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div> */}
       </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
